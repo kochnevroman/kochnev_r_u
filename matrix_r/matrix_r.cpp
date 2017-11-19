bb4432ab00c188ef3f5ffa4d08dc05b9c0dddadb
Unknown <co4new.roma@yandex.ru>
2017-11-19 23:19:55 +0300

    in process matrix_r

--- matrix_r/matrix_r.cpp
+++ matrix_r/matrix_r.cpp
@@ -1,7 +1,11 @@
 #include "matrix_r.h"
 #include <iostream>
 
-Matrix_R::Matrix_R()
+Matrix_R::Matrix_R(const double nRow, const double nCow)
+{
+}
+
+Matrix_R::Matrix_R(const Matrix_R &matrix_r)
 {
 
 }
@@ -11,12 +15,61 @@ Matrix_R::~Matrix_R()
 
 }
 
+double& Matrix_R::at(const ptrdiff_t iRow, const ptrdiff_t iCol) {
+
+}
+
+const double& Matrix_R::at(const ptrdiff_t iRow, const ptrdiff_t iCol) const {
+
+}
+
+//операции матрицы с матрицей
+Matrix_R Matrix_R::operator=(const Matrix_R &matrix_r) {
+
+}
+
+Matrix_R Matrix_R::operator+=(const Matrix_R &matrix_r) {
+
+}
+
+Matrix_R Matrix_R::operator-=(const Matrix_R &matrix_r) {
+
+}
+
+Matrix_R Matrix_R::operator*=(const Matrix_R &matrix_r) {
+
+}
+
+//операции матрицы с числом
+Matrix_R Matrix_R::operator+=(const double &number) {
+
+}
+
+Matrix_R Matrix_R::operator-=(const double &number) {
+
+}
+
+Matrix_R Matrix_R::operator*=(const double &number) {
+
+}
+
+void Matrix_R::determinant() {
+
+}
+
+void Matrix_R::transpose() {
+
+}
+
+void Matrix_R::resize(const ptrdiff_t &newNumberOfLines, const ptrdiff_t & newNumberOfColumns) {
+
+}
 
 std::ostream& Matrix_R::writeTo(std::ostream& ostrm) const
 {
     for (ptrdiff_t i = 0; i < nRow_; i++)
     {
-        for (ptrdiff_t j = 0; j < nCow_; j++)
+        for (ptrdiff_t j = 0; j < nCol_; j++)
         {
             ostrm << pRows_[i][j] << ' ';
         }
--- matrix_r/matrix_r.h
+++ matrix_r/matrix_r.h
@@ -10,15 +10,25 @@ class Matrix_R
 public:
     explicit Matrix_R() = default;
     Matrix_R(const double nRow, const double nCol);
-    Matrix_R(const Matrix_R& rhs);
+    Matrix_R(const Matrix_R& matrix_r);
     ~Matrix_R();
 
     double& at(const ptrdiff_t iRow, const ptrdiff_t iCol);
     const double& at(const ptrdiff_t iRow, const ptrdiff_t iCol) const;
 
+    //операции матрицы с матрицей
     Matrix_R operator=(const Matrix_R& matrix_r);
     Matrix_R operator+=(const Matrix_R& matrix_r);
     Matrix_R operator-=(const Matrix_R& matrix_r);
+    Matrix_R operator*=(const Matrix_R& matrix_r);
+
+    //операции матрицы с вектором
+    //
+
+    //операции матрицы с числом
+    Matrix_R operator+=(const double& number);
+    Matrix_R operator-=(const double& number);
+    Matrix_R operator*=(const double& number);
 
     const ptrdiff_t getnRow() const;
     const ptrdiff_t getnCol() const;
@@ -27,16 +37,36 @@ public:
 
 private:
     ptrdiff_t  nRow_ { 0 };
-    ptrdiff_t  nCow_ { 0 };
+    ptrdiff_t  nCol_ { 0 };
     int** pRows_ ( nullptr );
+
+    void determinant();
+    void transpose();
+    void resize(const ptrdiff_t& newNumberOfLines, const ptrdiff_t& newNumberOfColumns);
 };
 
-Matrix_R operator*(const Matrix_R& m, const Matrix_R& n);
+//операции матрицы с матрицей
+Matrix_R operator+(const Matrix_R& matrix1, const Matrix_R& matrix2);
+Matrix_R operator-(const Matrix_R& matrix1, const Matrix_R& matrix2);
+Matrix_R operator*(const Matrix_R& matrix1, const Matrix_R& matrix2);
+
+//операции матрицы с вектором
+//
+
+//операции матрицы с числом
+Matrix_R operator+(const Matrix_R& matrix1, const double& number);
+Matrix_R operator+(const double& number, const Matrix_R& matrix2);
+
+Matrix_R operator-(const Matrix_R& matrix1, const double& number);
+Matrix_R operator-(const double& number, const Matrix_R& matrix2);
+
+Matrix_R operator*(const Matrix_R& matrix1, const double& number);
+Matrix_R operator*(const double& number, const Matrix_R& matrix2);
 
 
-inline std::ostream& operator<<(std::ostream& ostrm, const Matrix_R& rhs)
+inline std::ostream& operator<<(std::ostream& ostrm, const Matrix_R& matrix_r)
 {
-    return rhs.writeTo(ostrm);
+    return matrix_r.writeTo(ostrm);
 }
 
 #endif //MATRIX_R_MATRIX_R_H
