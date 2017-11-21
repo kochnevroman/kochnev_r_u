67369d476da634c3762106840aab0ceeb49670be
Unknown <co4new.roma@yandex.ru>
2017-11-21 12:13:58 +0300

    in process matrix_r

--- matrix_r/matrix_r.cpp
+++ matrix_r/matrix_r.cpp
@@ -1,18 +1,52 @@
 #include "matrix_r.h"
 #include <iostream>
+#include <stdexcept>
 
-Matrix_R::Matrix_R(const double nRow, const double nCow)
-{
+Matrix_R::Matrix_R(const ptrdiff_t &nRow, const ptrdiff_t &nCol) {
+    if (nRow < 0 || nCol < 0)
+    {
+        throw ("Количество строк или столбцов не может быть отрицательным");
+    }
+    else {
+        nRow_ = nRow;
+        nCol_ = nCol;
+
+        pdata_ = new double*[nRow_];
+        pdata_[0] = new double[nRow_ * nCol_];
+        for (ptrdiff_t i = 0; i < nCol_ - 2; i += 1)
+        {
+            pdata_[i + 1] = pdata_[i] + nCol_;
+        }
+    }
+
 }
 
 Matrix_R::Matrix_R(const Matrix_R &matrix_r)
 {
+    nCol_ = matrix_r.nCol_;
+    nRow_ = matrix_r.nRow_;
+    pdata_ = new double* [nRow_];
 
+    for (ptrdiff_t i = 0; i < nRow_; i++)
+    {
+        pdata_[i] = new double[nCol_];
+    }
+
+    for (ptrdiff_t i = 0; i < nRow_; i++)
+    {
+        for (ptrdiff_t j = 0; j < nCol_; j++)
+        {
+            pdata_[i][j] = matrix_r.pdata_[i][j];
+        }
+    }
 }
 
 Matrix_R::~Matrix_R()
 {
-
+    nRow_ = 0;
+    nCol_ = 0;
+    delete[] pdata_;
+    pdata_ = nullptr;
 }
 
 double& Matrix_R::at(const ptrdiff_t iRow, const ptrdiff_t iCol) {
@@ -24,16 +58,60 @@ const double& Matrix_R::at(const ptrdiff_t iRow, const ptrdiff_t iCol) const {
 }
 
 //операции матрицы с матрицей
-Matrix_R Matrix_R::operator=(const Matrix_R &matrix_r) {
+Matrix_R Matrix_R::operator=(const Matrix_R &matrix_r)
+{
+    nCol_ = matrix_r.nCol_;
+    nRow_ = matrix_r.nRow_;
+    pdata_ = new double* [nRow_];
 
+    for (ptrdiff_t i = 0; i < nRow_; i += 1)
+    {
+        pdata_[i] = new double[nCol_];
+    }
+    for (ptrdiff_t i = 0; i < nRow_; i += 1)
+    {
+        for (ptrdiff_t j = 0; j < nCol_; j += 1)
+        {
+            pdata_[i][j] = matrix_r.pdata_[i][j];
+        }
+    }
+
+    return *this;
 }
 
-Matrix_R Matrix_R::operator+=(const Matrix_R &matrix_r) {
+Matrix_R Matrix_R::operator+=(const Matrix_R &matrix_r)
+{
+    if (nRow_ == matrix_r.nRow_ || nCol_ == matrix_r.nRow_)
+    {
+        for (ptrdiff_t i = 0; i < nRow_; i++)
+        {
+            for (ptrdiff_t j = 0; j < nCol_; j++)
+            {
+                pdata_[i][j] += matrix_r.pdata_[i][j];
+            }
+        }
+    }
 
+    else {
+        throw ("Матрицы разного размера");
+    }
 }
 
 Matrix_R Matrix_R::operator-=(const Matrix_R &matrix_r) {
+    if (nRow_ == matrix_r.nRow_ || nCol_ == matrix_r.nRow_)
+    {
+        for (ptrdiff_t i = 0; i < nRow_; i++)
+        {
+            for (ptrdiff_t j = 0; j < nCol_; j++)
+            {
+                pdata_[i][j] -= matrix_r.pdata_[i][j];
+            }
+        }
+    }
 
+    else {
+        throw ("Матрицы разного размера");
+    }
 }
 
 Matrix_R Matrix_R::operator*=(const Matrix_R &matrix_r) {
@@ -41,16 +119,18 @@ Matrix_R Matrix_R::operator*=(const Matrix_R &matrix_r) {
 }
 
 //операции матрицы с числом
-Matrix_R Matrix_R::operator+=(const double &number) {
-
-}
-
-Matrix_R Matrix_R::operator-=(const double &number) {
-
-}
-
 Matrix_R Matrix_R::operator*=(const double &number) {
 
+}
+
+const ptrdiff_t& Matrix_R::nRow() const
+{
+    return nRow_;
+}
+
+const ptrdiff_t& Matrix_R::nCol() const
+{
+    return nCol_;
 }
 
 void Matrix_R::determinant() {
@@ -65,13 +145,63 @@ void Matrix_R::resize(const ptrdiff_t &newNumberOfLines, const ptrdiff_t & newNu
 
 }
 
+void Matrix_R::addRow(const ptrdiff_t &newRowIndex, const double &*newRow) {
+
+}
+
+void Matrix_R::addCol(const ptrdiff_t &newColIndex, const int &*newCol) {
+
+}
+
+void Matrix_R::removeRow(const ptrdiff_t &removableRowIndex) {
+
+}
+
+void Matrix_R::removeCol(const ptrdiff_t &removableColIndex) {
+
+}
+
+void Matrix_R::multiplyNumberToRow(const double &number) {
+
+}
+
+void Matrix_R::multiplyNumberToCol(const double &number) {
+
+}
+
+void Matrix_R::reshuffleTwoRows(const ptrdiff_t &firstRow, const ptrdiff_t &secondRow) {
+
+}
+
+void Matrix_R::reshuffleTwoCols(const ptrdiff_t &firstCol, const ptrdiff_t &secondCol) {
+
+}
+
+Matrix_R operator+(const Matrix_R& firstMatrix, const Matrix_R& secondMatrix)
+{
+    Matrix_R matrix_r(firstMatrix);
+    return  matrix_r += secondMatrix;
+}
+
+Matrix_R operator-(const Matrix_R& firstMatrix, const Matrix_R& secondMatrix)
+{
+    Matrix_R matrix_r(firstMatrix);
+    return  matrix_r -= secondMatrix;
+}
+
+Matrix_R operator*(const Matrix_R& firstMatrix, const Matrix_R& secondMatrix)
+{
+    Matrix_R matrix_r(firstMatrix);
+    return  matrix_r *= secondMatrix;
+}
+
 std::ostream& Matrix_R::writeTo(std::ostream& ostrm) const
 {
     for (ptrdiff_t i = 0; i < nRow_; i++)
     {
         for (ptrdiff_t j = 0; j < nCol_; j++)
         {
-            ostrm << pRows_[i][j] << ' ';
+            ostrm << pdata_[i][j] << ' ';
         }
         ostrm << "\n";
     }
--- matrix_r/matrix_r.h
+++ matrix_r/matrix_r.h
@@ -9,8 +9,9 @@ class Matrix_R
 {
 public:
     explicit Matrix_R() = default;
-    Matrix_R(const double nRow, const double nCol);
+    Matrix_R(const ptrdiff_t& nRow, const ptrdiff_t& nCol);
     Matrix_R(const Matrix_R& matrix_r);
+
     ~Matrix_R();
 
     double& at(const ptrdiff_t iRow, const ptrdiff_t iCol);
@@ -26,40 +27,55 @@ public:
     //
 
     //операции матрицы с числом
-    Matrix_R operator+=(const double& number);
-    Matrix_R operator-=(const double& number);
     Matrix_R operator*=(const double& number);
-
-    const ptrdiff_t getnRow() const;
-    const ptrdiff_t getnCol() const;
-
-    std::ostream& writeTo(std::ostream& ostrm ) const;
 
 private:
     ptrdiff_t  nRow_ { 0 };
     ptrdiff_t  nCol_ { 0 };
-    int** pRows_ ( nullptr );
+    double** pdata_ { nullptr };
+
+    const ptrdiff_t& nRow() const;
+    const ptrdiff_t& nCol() const;
 
     void determinant();
     void transpose();
     void resize(const ptrdiff_t& newNumberOfLines, const ptrdiff_t& newNumberOfColumns);
+
+    //добавление строки
+    void addRow(const ptrdiff_t& newRowIndex, const double& newRow[]);
+    //добавление столбца
+    void addCol(const ptrdiff_t& newColIndex, const int& newCol[]);
+
+    //удаление строки
+    void removeRow(const ptrdiff_t& removableRowIndex);
+    //удаление столбца
+    void removeCol(const ptrdiff_t& removableColIndex);
+
+    //умножение на число строки
+    void multiplyNumberToRow(const double& number);
+
+    void multiplyNumberToCol(const double& number);
+    //умножение на число столбца
+
+    //перестановка двух строк
+    void reshuffleTwoRows(const ptrdiff_t& firstRow, const ptrdiff_t& secondRow);
+
+    //перестановка двух столбцов
+    void reshuffleTwoCols(const ptrdiff_t& firstCol, const ptrdiff_t& secondCol);
+
+
+    std::ostream& writeTo(std::ostream& ostrm ) const;
 };
 
 //операции матрицы с матрицей
-Matrix_R operator+(const Matrix_R& matrix1, const Matrix_R& matrix2);
-Matrix_R operator-(const Matrix_R& matrix1, const Matrix_R& matrix2);
-Matrix_R operator*(const Matrix_R& matrix1, const Matrix_R& matrix2);
+Matrix_R operator+(const Matrix_R& firstMatrix, const Matrix_R& secondMatrix);
+Matrix_R operator-(const Matrix_R& firstMatrix, const Matrix_R& secondMatrix);
+Matrix_R operator*(const Matrix_R& firstMatrix, const Matrix_R& secondMatrix);
 
 //операции матрицы с вектором
 //
 
 //операции матрицы с числом
-Matrix_R operator+(const Matrix_R& matrix1, const double& number);
-Matrix_R operator+(const double& number, const Matrix_R& matrix2);
-
-Matrix_R operator-(const Matrix_R& matrix1, const double& number);
-Matrix_R operator-(const double& number, const Matrix_R& matrix2);
-
 Matrix_R operator*(const Matrix_R& matrix1, const double& number);
 Matrix_R operator*(const double& number, const Matrix_R& matrix2);
 
