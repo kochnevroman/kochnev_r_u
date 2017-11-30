0040f08afeee116ea092dad259a9a74fcea0c852
Unknown <co4new.roma@yandex.ru>
2017-11-30 22:50:25 +0300

    developing priorityqueuel

--- priorityqueuel/priorityqueuel.cpp
+++ priorityqueuel/priorityqueuel.cpp
@@ -1,2 +1,49 @@
 #include "priorityqueuel.h"
 #include <iostream>
+
+PriorityQueueL::PriorityQueueL(const PriorityQueueL &priorityQueueL)
+{
+
+}
+
+PriorityQueueL::~PriorityQueueL()
+{
+    while( !isEmpty() ) {
+        pop();
+    }
+}
+
+PriorityQueueL& PriorityQueueL::operator=(const PriorityQueueL &priorityQueueL)
+{
+
+}
+
+void PriorityQueueL::push(const int &value)
+{
+
+}
+
+void PriorityQueueL::pop()
+{
+
+}
+
+int& PriorityQueueL::top()
+{
+
+}
+
+const int& PriorityQueueL::top() const
+{
+
+}
+
+bool PriorityQueueL::isEmpty() const
+{
+    return  pTail_ == nullptr;
+}
+
+std::ostream& PriorityQueueL::writeTo(std::ostream &ostrm)
+{
+
+}
--- priorityqueuel/priorityqueuel.h
+++ priorityqueuel/priorityqueuel.h
@@ -7,9 +7,39 @@
 class PriorityQueueL
 {
 public:
+    PriorityQueueL() = default;
+    explicit PriorityQueueL(const PriorityQueueL& priorityQueueL);
+
+    ~PriorityQueueL();
+
+    PriorityQueueL&operator=(const PriorityQueueL& priorityQueueL);
+
+    void push(const int& value);
+
+    void pop();
+
+    int& top();
+    const int& top() const;
+
+    bool isEmpty() const;
+
+    std::ostream& writeTo(std::ostream& ostrm);
 
 private:
+    struct Node
+    {
+        Node(Node * pNext, const int& value);
+        Node* pNext_{ nullptr };
+        int data_{ int(0) };
+    };
 
+    Node* pHead_{ nullptr };
+    Node* pTail_{ nullptr };
 };
+
+inline std::ostream& operator<<(std::ostream& ostrm, PriorityQueueL& priorityQueueL)
+{
+    return  priorityQueueL.writeTo(ostrm);
+}
 
 #endif //PRIORITYQUEUEL_PRIORITYQUEUEL_H
